13bbe26eb1d2e2ffa92370bb3ab1d9fbca76b323
Anders Nelson <anders.f.nelson@gmail.com>
2021-10-27 10:14:35 -0500

    working on app functionality

--- GeometryCalcWeb.py
+++ GeometryCalcWeb.py
@@ -6,11 +6,12 @@
 #
 # Author: Joe Axberg
 # Orig Date: 9/22/2021
-#
+# cylinder, sphere, cube, cone
 # imports
 from flask import Flask, request, render_template, redirect, url_for
 import cylinder #<--why?
-
+import cone
+import sphere
 #flask plumbing
 app = Flask(__name__)
 
@@ -21,12 +22,17 @@ def mainForm():
    if request.method == "POST":
       sphere = request.form.get("sphere")
       cylinder = request.form.get("cylinder")
+      cone = request.form.get("cone")
       print("Selection was: ", sphere, cylinder) #prints to command line for trouble shooting
-      if sphere == "on":
-         print("User selected sphere") #prints to command line for trouble shooting
+      if cone == "on":
+         print("User selected cone") #prints to command line for trouble shooting
+         return redirect(url_for('coneForm'))
       elif cylinder == "on":
          print("User selected cylinder") #prints to command line for trouble shooting
          return redirect(url_for('cylinderForm'))
+      elif sphere == "on":
+         print("User selected sphere") #prints to command line for trouble shooting
+         return redirect(url_for('sphereForm'))
    return render_template("index.html")
 
 #flask route for the cylinder calculations page
@@ -42,6 +48,28 @@ def cylinderForm():
    return render_template("cylinder.html")
 
 #more code here for the rest of the calculators: sphere, cube, etc.
+
+@app.route("/cone", methods = ["GET", "POST"])
+def coneForm():
+   if request.method == "POST":
+       # getting input with name = fname in HTML form
+       radius = request.form.get("rad")
+       # getting input with name = lname in HTML form 
+       height = request.form.get("hgt") 
+       vol = cone.volume(int(radius), int(height))
+       return "User entered: Radius "+ str(radius) + " and Height: " + str(height) + ". <p>The Volume is: " + str(vol)
+   return render_template("cone.html")
+
+@app.route("/sphere", methods = ["GET", "POST"])
+def sphereForm():
+   if request.method == "POST":
+       # getting input with name = fname in HTML form
+       radius = request.form.get("rad")
+       # getting input with name = lname in HTML form 
+       height = request.form.get("hgt") 
+       vol = cone.volume(int(radius), int(height))
+       return "User entered: Radius "+ str(radius) + " and Height: " + str(height) + ". <p>The Volume is: " + str(vol)
+   return render_template("sphere.html")
   
 if __name__=='__main__':   #more flask plumbing so the environment starts correctly
-   app.run(host='0.0.0.0')
+   app.run(host='0.0.0.0', debug=True)
